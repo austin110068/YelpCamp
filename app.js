5ce433ea575796cb71870d37256540930e81f9d6
Chien-Yu Chen <feather@Chien-Yus-MacBook-Pro.local>
2021-06-05 01:07:10 -0700

    Adding in Authentication.

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
+// Connecting to Database
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -23,6 +28,7 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+// Configuration for App
 const app = express();
 const PORT = 3000;
 
@@ -30,10 +36,12 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Express Middleware
 app.use(express.urlencoded({extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Session
 const sessionConfig = {
     secret: "thisshouldbebettersecret",
     resave: false,
@@ -47,9 +55,19 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+// Setting Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // Setting Flash Middleware
 // Must be before Routing
 app.use((req, res, next) => {
+    console.log(req.session);
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
@@ -57,8 +75,9 @@ app.use((req, res, next) => {
 
 // Routing
 // Default route with ./campgrounds
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 // Home Page
 app.get('/', (req, res) => {
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const { campgroundSchema } = require('../schemas.js');
+const { isLoggedIn } = require('../middleware');
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
@@ -23,11 +24,11 @@ router.get('/', catchAsync(async (req, res) => {
 // Make New Campground Page 
 // Frist GET the page, then POST a new page by redirecting
 // Be careful that this GET should be placed before ":id" because "new" could be considered as ":id", which means it would not work normally
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
 })
 
-router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     req.flash('success', 'Successfully made a new campground');
     const campground = new Campground(req.body.campground);  // we're passing ".campground" b.c. of the input we get from the form in new.ejs --> campground[title] / campground[location]
     await campground.save();
@@ -47,7 +48,7 @@ router.get('/:id', catchAsync(async (req, res) => {
 
 // Edit Campground Page
 // Frist GET the page, then PUT the edited page by redirecting.
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     if (!campground) {
         req.flash('error', 'Cannot find that campground...');
@@ -56,7 +57,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground });
 }))
 
-router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground});
     req.flash('success', 'Successfully updated campground!');
@@ -64,7 +65,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
 }))
 
 // Delete Campground Page
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted campground!');

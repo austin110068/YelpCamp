2fe9414f007387508aee28249145bfcc1b7ecb46
Chien-Yu Chen <feather@Chien-Yus-MacBook-Pro.local>
2021-06-08 20:52:35 -0700

    Upgrade to fancy cluster map.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,20 +1,21 @@
-const validCampground = JSON.parse(campground);
-console.log(validCampground);
+// const validCampground = JSON.parse(campground);
 
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: "map", 
-    style: "mapbox://styles/mapbox/streets-v11", // style URL
-    center: validCampground.geometry.coordinates, // starting position [lng, lat]
+    container: 'map', 
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9, // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
-    .setLngLat(validCampground.geometry.coordinates)
+    .setLngLat(campground.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({offset: 25})
         .setHTML(
-            `<h3>${validCampground.title}</h3><p>${validCampground.location}</p>`
+            `<h3>${campground.title}</h3><p>${campground.location}</p>`
         )
     )
     .addTo(map)
--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,14 @@
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-  }
+// if (process.env.NODE_ENV !== "production") {
+//     require('dotenv').config();
+// }
 
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
-const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mapBoxToken = process.env.MAPBOX_TOKEN;
-const geocoder = mbxGeocoding({accessToken: mapBoxToken});
+// const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+// const mapBoxToken = process.env.MAPBOX_TOKEN;
+// const geocoder = mbxGeocoding({accessToken: mapBoxToken});
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -26,31 +26,38 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 500; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const name = `${sample(descriptors)} ${sample(places)}`;
+        // const name = `${sample(descriptors)} ${sample(places)}`;
         const price = Math.floor(Math.random() * 20) + 10;
-        const location = `${cities[random1000].city}, ${cities[random1000].state}`
-        const geoData = await geocoder.forwardGeocode({
-            query: location,
-            limit: 1
-        }).send()
+        // const location = `${cities[random1000].city}, ${cities[random1000].state}`
+        // const geoData = await geocoder.forwardGeocode({
+        //     query: location,
+        //     limit: 1
+        // }).send()
 
         const camp = new Campground({
             author: '60bb2d558a4271b30740cf93',
-            location: location,
-            title: name,
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
             description: 'lorem',
             price,
-            geometry: geoData.body.features[0].geometry,
+            // geometry: geoData.body.features[0].geometry,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/yelpcampproject110068/image/upload/v1623023372/YelpCamp/sj2f3p8dzsmdyamkdxuc.jpg',
                     filename: 'YelpCamp/sj2f3p8dzsmdyamkdxuc'
                 },
                 {
-                    url: 'https://res.cloudinary.com/yelpcampproject110068/image/upload/v1623023372/YelpCamp/nfs77tgmcehedzhqfwq2.jpg',
-                    filename: 'YelpCamp/nfs77tgmcehedzhqfwq2'
+                    url: 'https://res.cloudinary.com/yelpcampproject110068/image/upload/v1623024950/YelpCamp/jdmosf4s4eyv6mcgrryh.jpg',
+                    filename: 'YelpCamp/jdmosf4s4eyv6mcgrryh'
                 }
             ]
         });
